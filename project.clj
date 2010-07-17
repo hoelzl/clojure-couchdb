2e186f5dbedab5276eebc74bf9d6db1859dc474e
Matthias Hölzl <hoelzl@agilogik.de>
2010-07-17 22:59:26 +0200

    Updated to Clojure 1.2 beta1.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject org.clojars.tc/clojure-couchdb "0.2.2-tc"
+(defproject org.clojars.tc/clojure-couchdb "0.2.2.1-tc"
   :description "Simple Clojure interface to Apache CouchDB, fork of the original project with function arguments instead of *server* and some other changes.  Updated for Clojure 1.2"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-		 [org.clojure/clojure-contrib "1.2.0-master-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-beta1"]
+		 [org.clojure/clojure-contrib "1.2.0-beta1"]
 		 [clojure-http-client "1.0.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]])
